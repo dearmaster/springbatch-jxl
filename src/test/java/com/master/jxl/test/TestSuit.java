5318852bd0afa597b641b2b1971a8ea3dfdacb97
dearmaster windows7 <694114533@qq.com>
2018-01-07 21:55:44 +0800

    add test suite

---
+++ src/test/java/com/master/jxl/test/TestSuit.java
@@ -0,0 +1,12 @@
+package com.master.jxl.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        EmployeeExportTest.class,
+        StudentExportTest.class
+})
+public class TestSuit {
+}
